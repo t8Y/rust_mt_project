56da22f97a90d1ebf306f07579ddda03a40fc96a
takahiro <2tacatakataca2@googlemail.com>
2016-10-17 00:10:23 +0900

    cargo init

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-
-    }
+#[no_mangle]
+pub extern fn square(x: i32) -> i32 {
+    x * x
 }
+
+//#[cfg(test)]
+//mod tests {
+//    #[test]
+//    fn it_works() {
+//
+//    }
+//}
