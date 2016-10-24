c174cd8853e3a96debb508854521ead1acae8696
takahiro <2tacatakataca2@googlemail.com>
2016-10-25 00:23:10 +0900

    refresh

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #[no_mangle]
+#[export_name="square"]
 pub extern "system" fn square(x: i32) -> i32 {
     x * x
 }
+
+
+//#[export_name="\x01square"]
 
 //pub extern fn square(x: i32) -> i32 {
 //    x * x
